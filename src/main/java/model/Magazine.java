43b0edf4e87aeb5c8565c03e9203a3e20e31058c
Mikołaj <m.wietecki@icloud.com>
2024-02-20 23:53:27 +0100

    Changed the types of fields storing day, month, year using LocalDate

--- src/main/java/model/Magazine.java
+++ src/main/java/model/Magazine.java
@@ -1,40 +1,38 @@
 package model;
 
+import java.time.MonthDay;
+import java.util.Objects;
+
 public class Magazine extends Publication {
     public static final String TYPE = "Magazyn";
-    private int month;
-    private int day;
+    private MonthDay monthDay;
     private String language;
 
     public Magazine(String title, String publisher, String language, int year, int month, int day) {
         super(title, publisher, year);
         this.language = language;
-        this.month = month;
-        this.day = day;
+        this.monthDay = MonthDay.of(month,day);
     }
 
-    public int getMonth() {
-        return month;
+    public MonthDay getMonthDay() {
+        return monthDay;
     }
-    public void setMonth(int month) {
-        this.month = month;
+
+    public void setMonthDay(MonthDay monthDay) {
+        this.monthDay = monthDay;
     }
-    public int getDay() {
-        return day;
-    }
-    public void setDay(int day) {
-        this.day = day;
-    }
+
     public String getLanguage() {
         return language;
     }
+
     public void setLanguage(String language) {
         this.language = language;
     }
 
     @Override
     public String toString() {
-        return super.toString() + ", " + month + ", " + day + ", " + language;
+        return super.toString() + ", " + monthDay.getMonthValue() + ", " + monthDay.getDayOfMonth() + ", " + language;
     }
 
     @Override
@@ -43,8 +41,8 @@ public class Magazine extends Publication {
                 getTitle() + ";" +
                 getPublisher() + ";" +
                 getYear() + ";" +
-                month + ";" +
-                day + ";" +
+                monthDay.getMonthValue() + ";" +
+                monthDay.getDayOfMonth() + ";" +
                 language;
     }
 }
